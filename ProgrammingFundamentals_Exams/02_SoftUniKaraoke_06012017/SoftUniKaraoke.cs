b2052502a7f04639a2f5aeaa354965236db60a63
Milena Petkanova <milenaslaveikova78@gmail.com>
2017-11-04 21:44:03 +0200

    70/100 when tested

---
+++ ProgrammingFundamentals_Exams/02_SoftUniKaraoke_06012017/SoftUniKaraoke.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SoftUniKaraoke
+{
+    static void Main()
+    {
+        var list = new List<string>();
+        var allParticipants = new SortedDictionary<string, List<string>>();
+        var awardsCounter = new SortedDictionary<string, int>();
+
+        string[] participants = Console.ReadLine()
+            .Split(',').Select(s => s.Trim()).ToArray();
+        string[] songs = Console.ReadLine()
+            .Split(',').Select(s => s.Trim()).ToArray();
+        string command = Console.ReadLine();
+
+        foreach (var participant in participants)
+        {
+            list.Add(participant);
+        }
+        foreach (var song in songs)
+        {
+            list.Add(song);
+        }
+
+        while (command != "dawn")
+        {
+            string[] performance = command
+                .Split(',').Select(s => s.Trim()).ToArray();
+            string name = performance[0];
+            string song = performance[1];
+            string award = performance[2];
+
+            if (list.Contains(name) && list.Contains(song))
+            {
+                if (!allParticipants.ContainsKey(name))
+                {
+                    allParticipants.Add(name, new List<string>());
+                    allParticipants[name].Add(award);
+                    awardsCounter.Add(name, 1);
+                }
+                else if (!allParticipants[name].Contains(award))
+                {
+                    allParticipants[name].Add(award);
+                    awardsCounter[name]++;
+                }
+            }
+
+            command = Console.ReadLine();
+        }
+
+        int counter = 0;
+        foreach (var participant in awardsCounter.OrderByDescending(x => x.Value))
+        {
+            Console.WriteLine("{0}: {1} awards", 
+                participant.Key, participant.Value);
+            foreach (var award in allParticipants[participant.Key].OrderBy(x => x))
+            {
+                Console.WriteLine("--{0}", award);
+                counter++;
+            }
+        }
+
+        if (counter == 0)
+        {
+            Console.WriteLine("No awards");
+        }
+    }
+}
---
+++ ProgrammingFundamentals_Exams/04_WinningTicket_06012017/WinningTicket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class WinningTicket
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine()
+            .Split(new char[] {',', ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+        foreach (var ticket in input)
+        {
+            if (ticket.Length != 20)
+            {
+                Console.WriteLine("invalid ticket");
+                return;
+            }
+
+            else if (Regex.IsMatch(ticket, @"([@]{20})")
+                    || Regex.IsMatch(ticket, @"([#]{20})")
+                    || Regex.IsMatch(ticket, @"([$]{20})")
+                    || Regex.IsMatch(ticket, @"([/^]{20})"))
+            {
+                Console.WriteLine(@"ticket ""{0}"" - 10{1} Jackpot!",
+                    ticket, ticket.First());
+            }
+
+            else if (Regex.IsMatch(ticket, @"([@$#^]{6,9})(.*?)?\1"))
+            {
+                var match = Regex.Match(ticket, @"[@$#^]{6,9}");
+                int matchesCounter = match.Length;
+
+                Console.WriteLine(@"ticket ""{0}"" - {1}{2}",
+                    ticket, matchesCounter, ticket.ElementAt(5));
+            }
+
+            else
+            {
+                Console.WriteLine(@"ticket ""{0}"" - no match", ticket);
+            }
+        }
+    }
+}
