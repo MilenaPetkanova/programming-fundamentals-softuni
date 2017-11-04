abc81e6e142e52156436c9d364ae745546b791f7
Milena Petkanova <milenaslaveikova78@gmail.com>
2017-11-04 20:53:42 +0200

    100/100 testings

---
+++ ProgrammingFundamentals_Exams/03_HornetAssault_26022017/HornetAssault.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class HornetAssault
+{
+    static void Main()
+    {
+        List<long> beehives = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse).ToList();
+        List<long> hornets = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse).ToList();
+
+        int beehivesStartCount = beehives.Count;
+
+        for (int b = 0; b < beehivesStartCount; b++)
+        {
+            if (hornets.Count == 0)
+            {
+                break;
+            }
+
+            long hPower = hornets.Sum();
+            beehives[b] -= hPower;
+
+            if (beehives[b] < 0)
+            {
+                beehives[b] = 0;
+            }
+            else
+            {
+                hornets.RemoveAt(0);
+            }
+        }
+
+        if (beehives.Any(x => x != 0))
+        {
+            beehives.RemoveAll(x => x == 0);
+            Console.WriteLine(string.Join(" ", beehives));
+        }
+        else
+        {
+            Console.WriteLine(string.Join(" ", hornets));
+        }
+    }
+}
